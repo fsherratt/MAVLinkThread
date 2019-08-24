90c35db57a9794ebefc23abc85946859946a38ca
Freddie Sherratt <fsherratt@gmail.com>
2019-08-24 21:51:52 +0100

    Remove timeout from constructor arguments

--- mavlinkThread/mavSerial.py
+++ mavlinkThread/mavSerial.py
@@ -28,17 +28,16 @@ class mavSerial( commAbstract ):
     # param writeTimeout - serial write timeout
     # return void
     # --------------------------------------------------------------------------
-    def __init__( self, serialPortAddress, baudrate = 57600, readtimeout = 0.01,
-                  writeTimeout = 3 ):
+    def __init__( self, serialPortAddress, baudrate = 57600 ):
 
-        self._readTimeout = readtimeout
+        self._readTimeout = 0.01
 
         self._serialObj = serial.Serial()
 
         self._serialObj.port = serialPortAddress
         self._serialObj.baudrate = baudrate
         self._serialObj.timeout = readtimeout
-        self._serialObj.write_timeout = writeTimeout
+        self._serialObj.write_timeout = 3
 
         self._writeLock = threading.Lock()
         self._readLock = threading.Lock()
